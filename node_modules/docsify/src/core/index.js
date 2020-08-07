abf98bcce717f5910ed806aebd8a4b5feac98968
Teages <Teages@teages.xyz>
2020-08-07 18:32:10 +0800

    delete some trash

--- node_modules/docsify/src/core/index.js
+++
@@ -1,8 +0,0 @@
-import { documentReady } from './util/dom';
-import { Docsify } from './Docsify';
-
-/**
- * Run Docsify
- */
-// eslint-disable-next-line no-unused-vars
-documentReady(_ => new Docsify());
--- node_modules/docsify/src/core/render/compiler/taskListItem.js
+++
@@ -1,9 +0,0 @@
-export const taskListItemCompiler = ({ renderer }) =>
-  (renderer.listitem = text => {
-    const isTaskItem = /^(<input.*type="checkbox"[^>]*>)/.test(text);
-    const html = isTaskItem
-      ? `<li class="task-list-item"><label>${text}</label></li>`
-      : `<li>${text}</li>`;
-
-    return html;
-  });
