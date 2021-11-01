0432e3e31a3371d7b3a40d9142fe8aad6e28cf38
Teages <Teages@teages.xyz>
2021-11-01 08:47:44 +0800

    support ko

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,30 +1,10 @@
 const { fs, path } = require('@vuepress/shared-utils')
+const localesConfig = require('./locales')
 
 module.exports = ctx => ({
   evergreen: true,
   dest: './wiki-html',
-  locales: {
-    '/': {
-      lang: 'en-US',
-      title: 'Cytoid Wiki',
-      description: 'A wiki for Cytoid and powered by community.'
-    },
-    '/en/': {
-      lang: 'en-US',
-      title: 'Cytoid Wiki',
-      description: 'A wiki for Cytoid and powered by community.'
-    },
-    '/zh/': {
-      lang: 'zh-Hans',
-      title: 'Cytoid Wiki',
-      description: '社区驱动的 Cytoid Wiki.'
-    },
-    '/vi/': {
-      lang: 'vi',
-      title: 'Cytoid Wiki',
-      description: 'Một "trạm đỗ xe" dành cho Cytoid bởi cộng đồng!'
-    }
-  },
+  locales: localesConfig.locales,
   head: [
     ['link', { rel: 'icon', href: `/site-source/pic/cytoid-girl.png` }],
     ['link', { rel: 'stylesheet', href: '//cdn.jsdelivr.net/npm/katex@0.7.0/dist/katex.min.css' }],
@@ -52,60 +32,7 @@ module.exports = ctx => ({
     //   }
     // }) : null,
     smoothScroll: true,
-    locales: {
-      '/':{
-        label: 'Auto',
-        selectText: '',
-        ariaLabel: '',
-        editLinkText: '',
-        lastUpdated: ''
-      },
-      '/en/': {
-        label: 'English',
-        selectText: 'Languages',
-        ariaLabel: 'Select language',
-        editLinkText: 'Edit this page on GitHub',
-        lastUpdated: 'Last Update',
-        author: 'Author',
-        notFinish: 'Hmm... Something went wrong? Please wait... We have not done yet :(',
-        footer:  `<span><a href="https://github.com/CytoidCommunity/Cytoid-wiki"><strong>Cytoid Wiki Team</strong></a> ©2020. All rights reserved.</span>
-                  <span>Written by Discord Community and Localizers.</span>
-                  <span>This site is the community wiki of <a href="https://cytoid.io/" class=""><strong>Cytoid</strong></a> built by <a href="https://cytoid.io/profile/tigerhix" class=""><strong>TigerHix</strong></a> and <a href="https://cytoid.io/profile/neo" class=""><strong>Neo</strong></a>.</span>
-                  `,
-        nav: require('./nav/en'),
-        sidebar: require('./sidebar/en')
-      },
-      '/zh/': {
-        label: '中文',
-        selectText: '语言',
-        ariaLabel: '选择语言',
-        editLinkText: '在 GitHub 上编辑',
-        lastUpdated: '上次编辑',
-        author: '作者',
-        notFinish: '还没做完, 再咕咕咕一会(',
-        footer:  `<span><a href="https://github.com/CytoidCommunity/Cytoid-wiki"><strong>Cytoid Wiki Team</strong></a> ©2020. All rights reserved.</span>
-                  <span>Written by Discord Community and Localizers.</span>
-                  <span>This site is the community wiki of <a href="https://cytoid.io/" class=""><strong>Cytoid</strong></a> built by <a href="https://cytoid.io/profile/tigerhix" class=""><strong>TigerHix</strong></a> and <a href="https://cytoid.io/profile/neo" class=""><strong>Neo</strong></a>.</span>
-                  `,
-        nav: require('./nav/zh'),
-        sidebar: require('./sidebar/zh')
-      },
-      '/vi/': {
-        label: 'Tiếng Việt',
-        selectText: 'Ngôn ngữ',
-        ariaLabel: 'Chọn ngôn ngữ',
-        editLinkText: 'Chỉnh sửa bài viết này trên Github',
-        lastUpdated: 'Cập nhật lần cuối vào',
-        author: 'Được viết bởi:',
-        notFinish: 'Hmm... Không có nội dung gì cả! ... Xin vui lòng chờ, bài viết này chưa hoàn thành đâu!',
-        footer:  `<span><a href="https://github.com/CytoidCommunity/Cytoid-wiki"><strong>Cytoid Wiki Team</strong></a> ©2020. Toàn quyền bảo lưu.</span>
-                  <span>Được tạo ra bởi cộng đồng Cytoid trên Discord.</span>
-                  <span>Đây là trang wiki của cộng đồng <a href="https://cytoid.io/" class=""><strong>Cytoid</strong></a>, được phát triển bởi <a href="https://cytoid.io/profile/tigerhix" class=""><strong>TigerHix</strong></a> và <a href="https://cytoid.io/profile/neo" class=""><strong>Neo</strong></a>.</span>
-                  `,
-        nav: require('./nav/vi'),
-        sidebar: require('./sidebar/vi')
-      }
-    }
+    locales: localesConfig.themeLocales,
   },
   plugins: [
     ['@vuepress/pwa', {
